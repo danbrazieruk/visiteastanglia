cf7e2de77ab96a16746ca9a676e9c5243439e966
Dan Brazier <dan.brazier@icloud.com>
2018-07-30 15:38:56 +0100

    general config changes

--- config/general.php
+++ config/general.php
@@ -39,7 +39,7 @@ return [
     // Staging environment settings
     'staging' => [
         // Base site URL
-        'siteUrl' => null,
+        'siteUrl' => 'visitengland.danbrazier.co.uk',
     ],
 
     // Production environment settings
